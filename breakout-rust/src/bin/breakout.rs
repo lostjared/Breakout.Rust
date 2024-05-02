c6357fed6c927ba44d3562597b56b4b425425833
Jared Bruni <jaredbruni@gmail.com>
2024-05-02 15:23:01 -0700

    intro gfx

--- breakout-rust/src/bin/breakout.rs
+++ breakout-rust/src/bin/breakout.rs
@@ -4,7 +4,6 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::render::TextureQuery;
-use std::time::{SystemTime, UNIX_EPOCH};
 
 fn printtext(
     can: &mut sdl2::render::Canvas<sdl2::video::Window>,
@@ -34,6 +33,7 @@ fn main() {
     let width = 1440;
     let height = 1080;
     let sdl = sdl2::init().unwrap();
+    let timer_subsystem = sdl.timer().unwrap();
     let video = sdl.video().unwrap();
     let window = video
         .window("Breakout.Rust", width, height)
@@ -54,11 +54,13 @@ fn main() {
         .map_err(|e| e.to_string())
         .expect("Error on texture create");
     let mut e = sdl.event_pump().unwrap();
-
+    let intro_surface = sdl2::surface::Surface::load_bmp("./logo.bmp").unwrap();
+    let intro_texture = tc.create_texture_from_surface(intro_surface).unwrap();
     let mut breakout = Breakout::new();
     breakout.new_game();
-    let mut prev_tick = 0;
-    let mut tick_count = 0;
+    let mut ticks = timer_subsystem.ticks();
+    let mut total_tick: u32 = 0;
+    let mut intro = true;
     'main: loop {
         for _event in e.poll_iter() {
             match _event {
@@ -74,49 +76,57 @@ fn main() {
         can.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         can.clear();
 
-        for x in 0..TILE_W {
-            for y in 0..TILE_H {
-                let xpos = x * 32;
-                let ypos = y * 16;
-                let color = breakout.grid.color_from_type(&breakout.grid.blocks[x][y]);
-                can.set_draw_color(color);
-                can.draw_rect(sdl2::rect::Rect::new(xpos as i32, ypos as i32, 32, 16))
-                    .expect("draw rect");
+
+        let tick = timer_subsystem.ticks();
+        let tick_diff = tick-ticks;
+        ticks = tick;
+        total_tick += tick_diff;
+
+        if intro {
+            can.copy(&intro_texture, None, None).expect("on copy");
+        } else {
+            for x in 0..TILE_W {
+                for y in 0..TILE_H {
+                    let xpos = x * 32;
+                    let ypos = y * 16;
+                    let color = breakout.grid.color_from_type(&breakout.grid.blocks[x][y]);
+                    can.set_draw_color(color);
+                    can.draw_rect(sdl2::rect::Rect::new(xpos as i32, ypos as i32, 32, 16))
+                        .expect("draw rect");
+                }
             }
+
+            let xpos = breakout.paddle.x;
+            let ypos = breakout.paddle.y;
+            can.set_draw_color(breakout.paddle.color);
+            can.fill_rect(sdl2::rect::Rect::new(xpos, ypos, 200, 20))
+                .expect("on fill");
+            let xpos = breakout.ball.x;
+            let ypos = breakout.ball.y;
+            can.fill_rect(sdl2::rect::Rect::new(xpos, ypos, 16, 16))
+                .expect("on ball");
+
+            printtext(
+                &mut can,
+                &tc,
+                &font,
+                75,
+                75,
+                sdl2::pixels::Color::RGB(255, 255, 255),
+                &format!("Lives: {} Score: {}", breakout.lives, breakout.score),
+            );
         }
+        can.present();       
 
-        let xpos = breakout.paddle.x;
-        let ypos = breakout.paddle.y;
-        can.set_draw_color(breakout.paddle.color);
-        can.fill_rect(sdl2::rect::Rect::new(xpos, ypos, 200, 20))
-            .expect("on fill");
-        let xpos = breakout.ball.x;
-        let ypos = breakout.ball.y;
-        can.fill_rect(sdl2::rect::Rect::new(xpos, ypos, 16, 16))
-            .expect("on ball");
-
-        printtext(
-            &mut can,
-            &tc,
-            &font,
-            75,
-            75,
-            sdl2::pixels::Color::RGB(255, 255, 255),
-            &format!("Lives: {} Score: {}", breakout.lives, breakout.score),
-        );
-
-        can.present();
-
-        let start = SystemTime::now();
-        let se = start.duration_since(UNIX_EPOCH).expect("error on time");
-        let tick = se.as_secs() * 1000 + se.subsec_nanos() as u64 / 1_000_000;
-        let ptick = tick - prev_tick;
-        prev_tick = tick;
-        tick_count += ptick;
-
-        if tick_count > 10 {
+        if intro {
+            if total_tick > 2500 {
+                total_tick = 0;
+                intro = false;
+            }
+        } else if total_tick > 10 {
+            total_tick  = 0;
             breakout.update();
-            tick_count = 0;
+            
             let keyboard_state = e.keyboard_state();
             if keyboard_state.is_scancode_pressed(sdl2::keyboard::Scancode::Left) {
                 breakout.paddle.move_left();
