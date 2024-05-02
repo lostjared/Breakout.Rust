6227ab40096841565e749f62e81f103721d9a4fb
Jared Bruni <jaredbruni@gmail.com>
2024-05-02 01:45:21 -0700

    working on grid

--- breakout-rust/src/bin/breakout.rs
+++ breakout-rust/src/bin/breakout.rs
@@ -1,21 +1,36 @@
-
+use breakout_rust::breakout::*;
+use rand::Rng;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
-use rand::Rng;
 
 fn main() {
-    let width = 1280;
-    let height = 720;
+    let width = 1440;
+    let height = 1080;
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
-    let window = video.window("Breakout.Rust", width, height).resizable().opengl().build().unwrap();
-    let mut can = window.into_canvas().build().map_err(|e| e.to_string()).expect("Error on canvas");
+    let window = video
+        .window("Breakout.Rust", width, height)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut can = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())
+        .expect("Error on canvas");
     let tc = can.texture_creator();
-    let mut texture = tc.create_texture_streaming(PixelFormatEnum::RGB24, width, height).map_err(|e| e.to_string()).expect("Error on texture create");
+    let mut texture = tc
+        .create_texture_streaming(PixelFormatEnum::RGB24, width, height)
+        .map_err(|e| e.to_string())
+        .expect("Error on texture create");
     let mut e = sdl.event_pump().unwrap();
-    let mut rng = rand::thread_rng();
+
+    let mut breakout = Breakout::new();
+    breakout.new_game();
+
     'main: loop {
         for _event in e.poll_iter() {
             match _event {
@@ -27,10 +42,19 @@ fn main() {
                 _ => {}
             }
         }
-        can.set_draw_color(sdl2::pixels::Color::RGB(0,0,0));
+        can.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         can.clear();
         //can.copy(&texture, None, Some(Rect::new(0, 0, width, height))).expect("on copy");
+
+        for x in 0..TILE_W {
+            for y in 0..TILE_H {
+                let xpos = x * 32;
+                let ypos = y * 16;
+                let color = Grid::color_from_type(&breakout.grid.blocks[x][y]);
+                can.set_draw_color(color);
+                can.draw_rect(sdl2::rect::Rect::new(xpos as i32, ypos as i32, 32, 16)).expect("draw rect");
+            }
+        }
         can.present();
     }
-
 }
--- breakout-rust/src/lib.rs
+++ breakout-rust/src/lib.rs
@@ -1,7 +1,105 @@
-
 pub mod breakout {
 
-    
+    use rand::Rng;
 
+    pub struct Paddle {
+        x: i32,
+        y: i32,
+        color: sdl2::pixels::Color,
+    }
 
+    pub const TILE_W: usize = 1440 / 32;
+    pub const TILE_H: usize = 1080 / 2 / 16;
+
+    #[derive(Copy, Clone, Debug)]
+    pub struct Block {
+        pub color_type: u32,
+    }
+
+    pub struct Grid {
+        pub blocks: Box<[[Block; TILE_H]; TILE_W]>,
+    }
+
+    pub struct Breakout {
+        paddle: Paddle,
+        pub score: u32,
+        pub grid: Grid,
+    }
+
+    impl Paddle {
+        pub fn new(xpos: i32, ypos: i32) -> Paddle {
+            Paddle {
+                x: xpos,
+                y: ypos,
+                color: sdl2::pixels::Color::RGB(255, 255, 255),
+            }
+        }
+    }
+
+    impl Grid {
+        fn new() -> Grid {
+            let g = Box::new([[Block { color_type: 0 }; TILE_H]; TILE_W]);
+            Grid { blocks: g }
+        }
+
+        fn fill_rand(&mut self) {
+            let mut rng = rand::thread_rng();
+            for i in 0..TILE_W {
+                for z in 0..TILE_H {
+                    self.blocks[i][z].color_type = rng.gen_range(1..8);
+                }
+            }
+        }
+
+        pub fn color_from_type(b: &Block) -> sdl2::pixels::Color {
+            match b.color_type {
+                1 => {
+                    return sdl2::pixels::Color::RGB(255, 0, 0);
+                }
+
+                2 => {
+                    return sdl2::pixels::Color::RGB(255, 255, 0);
+                }
+
+                3 => {
+                    return sdl2::pixels::Color::RGB(255, 0, 25);
+                }
+
+                4 => {
+                    return sdl2::pixels::Color::RGB(255, 255, 255);
+                }
+
+                5 => {
+                    return sdl2::pixels::Color::RGB(255, 100, 50);
+                }
+
+                6 => {
+                    return sdl2::pixels::Color::RGB(255, 200, 100);
+                }
+
+                7 => {
+                    return sdl2::pixels::Color::RGB(255, 75, 125);
+                }
+                8 => {
+                    return sdl2::pixels::Color::RGB(255, 60, 20);
+                }
+                _ => {
+                    return sdl2::pixels::Color::RGB(0, 0, 0);
+                }
+            }
+        }
+    }
+
+    impl Breakout {
+        pub fn new() -> Self {
+            Breakout {
+                paddle: Paddle::new(1280 / 2 - (50), 500),
+                score: 0,
+                grid: Grid::new(),
+            }
+        }
+        pub fn new_game(&mut self) {
+            self.grid.fill_rand();
+        }
+    }
 }
